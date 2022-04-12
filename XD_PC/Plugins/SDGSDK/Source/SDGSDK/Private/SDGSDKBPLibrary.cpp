d87ec0c022c76888490d17727f545aec2bd54273
harryphone <harryphone@163.com>
2022-04-12 15:07:58 +0800

    feat: create XDGSDK Plugin

---
+++ XD_PC/Plugins/SDGSDK/Source/SDGSDK/Private/SDGSDKBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SDGSDKBPLibrary.h"
+#include "SDGSDK.h"
+
+USDGSDKBPLibrary::USDGSDKBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float USDGSDKBPLibrary::SDGSDKSampleFunction(float Param)
+{
+	return -1;
+}
+

73808cd4d923528e907196a837f57de56f85d5aa
harryphone <harryphone@163.com>
2022-04-20 14:36:30 +0800

    feat: data storage add aes encrypt

--- XD_PC/Plugins/XDGSDK/Source/XDGSDK/Tools/DataStorage.cpp
+++ XD_PC/Plugins/XDGSDK/Source/XDGSDK/Tools/DataStorage.cpp
@@ -2,20 +2,26 @@
 
 #include <functional>
 
+#include "TDSCrypto.h"
+
 TSharedPtr<FJsonObject> DataStorage::JsonObject = NULL;
 
-static FString DataStoragePath = FPaths::SandboxesDir() + TEXT("/TDS/DataStorage.json");
+static FString DataStoragePath = FPaths::SandboxesDir() + TEXT("/TDS/DataStorageContent");
+
+static FString DataStorageKey = "version1";
 
 TSharedPtr<FJsonObject>& DataStorage::GetJsonObject()
 {
 	if (JsonObject == NULL)
 	{
 		FString filePath = DataStoragePath;
-		FString JsonStr;
-		if(FFileHelper::LoadFileToString(JsonStr, *filePath))
+		TArray<uint8> data;
+		if(FFileHelper::LoadFileToArray(data, *filePath))
 		{
+			auto JsonStr = TDSCrypto::UTF8Encode(TDSCrypto::AesDecode(data, TDSCrypto::UTF8Encode(DataStorageKey)));
 			const TSharedRef<TJsonReader<>>& JsonReader = TJsonReaderFactory<>::Create(JsonStr);
 			FJsonSerializer::Deserialize(JsonReader, JsonObject);
+			// UE_LOG(LogTemp, Display, TEXT("JsonStr: %s"), *JsonStr);
 		}
 	}
 	if (JsonObject == NULL)
@@ -42,7 +48,8 @@ void DataStorage::SaveToFile()
 	FString jsonStr;
 	TSharedRef<TJsonWriter<>> Writer = TJsonWriterFactory<>::Create(&jsonStr);
 	FJsonSerializer::Serialize(JsonObject.ToSharedRef(), Writer);
-	FFileHelper::SaveStringToFile(jsonStr, *filePath);
+	auto data = TDSCrypto::AesEncode(TDSCrypto::UTF8Encode(jsonStr), TDSCrypto::UTF8Encode(DataStorageKey));
+	FFileHelper::SaveArrayToFile(data, *filePath);
 }
 
 

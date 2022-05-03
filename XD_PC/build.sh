0e717fb1e2d0c70705257213a995fc035e66b7ec
harryphone <harryphone@163.com>
2022-05-04 00:09:35 +0800

    feat: add build sh

---
+++ XD_PC/build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+export LANG="en_US.UTF-8"
+
+PROJECT_DIR=$(cd "$(dirname $0)";pwd)
+UEVersion="UE_4.27"
+ProjectName="XD_PC"
+PRODUCT_DIR=${PROJECT_DIR}/Product
+
+doBuild() {
+    sh /Users/Shared/Epic\ Games/${UEVersion}/Engine/Build/BatchFiles/RunUAT.sh \
+    BuildCookRun \
+    -nocompileeditor \
+    -installed \
+    -nop4 \
+    -project=${PROJECT_DIR}/${ProjectName}.uproject \
+    -cook \
+    -stage \
+    -archive \
+    -archivedirectory=${PRODUCT_DIR} \
+    -package \
+    -ue4exe=/Users/Shared/Epic\ Games/${UEVersion}/Engine/Binaries/Mac/UE4Editor.app/Contents/MacOS/UE4Editor \
+    -compressed \
+    -ddc=InstalledDerivedDataBackendGraph \
+    -pak -prereqs -nodebuginfo \
+    -targetplatform=$1 \
+    -build \
+    -target=${ProjectName} \
+    -clientconfig=$2 \
+    -utf8output
+}
+
+#cleanPlugin() {
+#    cd $1
+#    rm -rf Binaries
+#    rm -rf Intermediate
+#    cd ..
+#    cd ..
+#}
+
+
+
+rm -rf $PRODUCT_DIR
+#mkdir -p $PRODUCT_DIR
+
+#Win32,Win64,HoloLens,Mac,XboxOne,PS4,IOS,Android,HTML5,Linux,LinuxAArch64,AllDesktop,TVOS,Switch,Lumin
+#'Debug', 'DebugGame', 'Development', 'Shipping', 'Test'
+doBuild Win64 Shipping
+
+
+

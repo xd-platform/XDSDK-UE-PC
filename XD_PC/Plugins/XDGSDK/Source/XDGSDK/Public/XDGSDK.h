b2131d095dc99bf4e2eada9266eef802b7af804e
harryphone <harryphone@163.com>
2022-07-20 17:35:03 +0800

    feat: 更改版本号6.5.0

--- XD_PC/Plugins/XDGSDK/Source/XDGSDK/Public/XDGSDK.h
+++ XD_PC/Plugins/XDGSDK/Source/XDGSDK/Public/XDGSDK.h
@@ -5,8 +5,8 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-#define XDUESDK_VERSION_NUMBER "60301001"
-#define XDUESDK_VERSION "6.3.1"
+#define XDUESDK_VERSION_NUMBER "60500001"
+#define XDUESDK_VERSION "6.5.0"
 
 
 class XDGSDK_API FXDGSDKModule : public IModuleInterface

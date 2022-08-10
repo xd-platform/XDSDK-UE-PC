2e098761937dc938c8ed21352d7763e34de7e4b2
harryphone <harryphone@163.com>
2022-08-10 17:51:26 +0800

    fix：web材质加载bug

--- XD_PC/Plugins/TapCommon/Source/TapWebBrowser/Private/TUWebBrowserAssetManager.cpp
+++ XD_PC/Plugins/TapCommon/Source/TapWebBrowser/Private/TUWebBrowserAssetManager.cpp
@@ -11,8 +11,8 @@
 
 UTUWebBrowserAssetManager::UTUWebBrowserAssetManager(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer) ,
-	DefaultMaterial(FString(TEXT("/WebBrowserWidget/WebTexture_M.WebTexture_M"))) ,
-	DefaultTranslucentMaterial(FString(TEXT("/WebBrowserWidget/WebTexture_TM.WebTexture_TM")))
+	DefaultMaterial(FString(TEXT("/TapCommon/WebTexture_M.WebTexture_M"))) ,
+	DefaultTranslucentMaterial(FString(TEXT("/TapCommon/WebTexture_TM.WebTexture_TM")))
 {
 #if WITH_EDITOR || PLATFORM_ANDROID || PLATFORM_IOS
 	// Add a hard reference to UWebBrowserTexture, without this the WebBrowserTexture DLL never gets loaded on Windows.
